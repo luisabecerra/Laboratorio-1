3c77609d8ec3a6df587b16f9ce8791dd1971e152
RUBEN <RUBEN@RUBEN-PC>
2016-02-15 22:19:32 -0500

    Primero

---
+++ src/laboratorio1/Laboratorio1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio1;
+
+/**
+ *
+ * @author RUBEN
+ */
+public class Laboratorio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.until.Scanner lectura=
+                new java.until.Scanner(System.in);
+        int mayor =0, menor=0;
+        int a,b;
+        
+        System.out.println("Ingrese el valor de A");
+        a=lectura.nextInt();
+        
+        System.out.println("Ingrese elvalor de B");
+        b=lectura.nextInt();
+        
+            
+        }
+    }
+    
+
